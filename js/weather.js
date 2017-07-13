ce4de2028443b43aa53ffe8b80d2b93d0aa1d67a
Amy Bedinghaus <amyngbedinghaus@gmail.com>
2017-07-13 06:24:52 -0400

    Fixed .env and got location data from Google Geolocation API

--- js/weather.js
+++ js/weather.js
@@ -1,34 +1,57 @@
-/********Modify Data********/
 var latitude;
 var longitude;
 var temp;
 var state;
 var city;
 var weather;
-var apiKey = '2280c837c13b677ee9b01f581b131cce';
 
-//get geolocation
+
+/********Protect API keys********/
+//SET ENV Variable so it exists even if a person doesn't include their credentials.
+
+var env = {
+    googleApiKey:'',
+    darkSkyAPIKey:'',
+};
+// NOW  Import variables if present (from env.js)
+if(window.__env) {
+  Object.assign(env, window.__env);
+}
+
+/********Get Data********/
 $(document).ready(function() {
-
+//get geolocation
   if (navigator.geolocation) {
-   navigator.geolocation.getCurrentPosition(function(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    console.log(longitude);
-    //feed into Dark Sky
-    $.getJSON('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + apiKey + '/' + latitude + ',' + longitude + '?exclude=minutely,hourly,daily,alerts,flags',
-    function darkSky(result){
-      var jsonString = JSON.stringify(result)
-      console.log(jsonString);
-    });
-   });
-  }
+    navigator.geolocation.getCurrentPosition(function(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      console.log(latitude, longitude);
+      //feed into Dark Sky
+      $.getJSON('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + env.darkSkyAPIKey + '/' + latitude + ',' + longitude + '?exclude=minutely,hourly,daily,alerts,flags',
+        function darkSky(result){
+          var jsonStringDark = JSON.stringify(result);
+          jsonStringDark = JSON.parse(jsonStringDark);
+          console.log(jsonStringDark);
+        });
+    //feed into Google Maps API to find city/state
+      $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key='+env.googleApiKey,
+        function geoCode(result){
+          var jsonStringGeocode = JSON.stringify(result);
+          //jsonStringGeocode = JSON.parse(jsonStringGeocode);
+          console.log(jsonStringGeocode);
+        });
+      });
+ }
+
+ //read Dark Sky 'currently' 'icon'
+
+
+
+
+
 });
 
 
-//
-
-//feed into Google Maps API to find city/state
 
 //read Dark Sky 'currently' 'icon'
 
